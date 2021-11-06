33d346375bdab0585ce54f6d0922f26d993ebb12
Diogo Souza <diogosouzac@gmail.com>
2021-11-06 10:48:22 -0300

    Adding eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import '@/styles/index.scss'
-
 import 'slick-carousel/slick/slick.scss'
 import 'slick-carousel/slick/slick-theme.scss'
 import 'slick-carousel'
 import '@fortawesome/fontawesome-free/css/all.css'
 
-require('slick-carousel/slick/slick.js')
-require('@fortawesome/fontawesome-free/js/all.js')
-require('./js/main.js')
+import '@fortawesome/fontawesome-free/js/all'
+import './js/main'
